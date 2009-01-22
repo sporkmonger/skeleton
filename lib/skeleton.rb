bcfeba870d2b0cd2f36e6e38512a56864fdae8fb
Bob Aman <bob@sporkmonger.com>
2009-01-21 20:21:07 -0500

    Changed copyright date.

--- lib/skeleton.rb
+++ lib/skeleton.rb
@@ -1,5 +1,5 @@
 # ++
-# Skeleton, Copyright (c) 2008 Bob Aman
+# Skeleton, Copyright (c) 2009 Bob Aman
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
